d92fe6e47ee0c70aeb57934d06ca58e94fd0cc15
Roland Shoemaker <roland@golang.org>
2026-01-29 08:26:02 +0800

    crypto/tls: add verifiedChains expiration checking during resumption
    When resuming a session, check that the verifiedChains contain at least one chain that is still valid at the time of resumption. If not, trigger a new handshake.
    
    Updates #77113
    Updates #77357
    Updates CVE-2025-68121
    
    Change-Id: I14f585c43da17802513cbdd5b10c552d7a38b34e
    Reviewed-on: https://go-review.googlesource.com/c/go/+/739321
    Reviewed-by: Coia Prant <coiaprant@gmail.com>
    Reviewed-by: Filippo Valsorda <filippo@golang.org>
    Auto-Submit: Roland Shoemaker <roland@golang.org>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    Reviewed-on: https://go-review.googlesource.com/c/go/+/740003
    Reviewed-by: Nicholas Husin <husin@google.com>
    Reviewed-by: Damien Neil <dneil@google.com>
    Auto-Submit: Dmitri Shuralyov <dmitshur@google.com>
    Reviewed-by: Nicholas Husin <nsh@golang.org>

--- common.go
+++ common.go
@@ -1783,3 +1783,16 @@ func (e *CertificateVerificationError) Error() string {
 func (e *CertificateVerificationError) Unwrap() error {
 	return e.Err
 }
+
+// anyUnexpiredChain reports if at least one of verifiedChains is still
+// unexpired. If verifiedChains is empty, it returns false.
+func anyUnexpiredChain(verifiedChains [][]*x509.Certificate, now time.Time) bool {
+	for _, chain := range verifiedChains {
+		if len(chain) != 0 && !slicesContainsFunc(chain, func(cert *x509.Certificate) bool {
+			return now.Before(cert.NotBefore) || now.After(cert.NotAfter) // cert is expired
+		}) {
+			return true
+		}
+	}
+	return false
+}
